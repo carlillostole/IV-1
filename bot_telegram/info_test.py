bae91751687c905c2eb2f7f55972c70e6923adcd
Sergio Caceres <sergiocaceres@correo.ugr.es>
2016-11-07 17:11:25 +0100

    Actualizando test

--- bot_telegram/info_test.py
+++ bot_telegram/info_test.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*- 
 
-vecto = u'Pacheco (6 P.), Rami, Garay, Dani Castellano (10 P.), Luna (2 P.), \xc1lex Berenguer (2 P.), Reyes, Moi G\xf3mez (2 P.), Franco V\xe1zquez, Guerrero, Deyverson'
+vecto = u'Pacheco (6 P.), Rami (6 P.), Garay (6 P.), Dani Castellano (10 P.), Luna (2 P.), \xc1lex Berenguer (2 P.), Reyes (6 P.), Moi G\xf3mez (2 P.), Franco V\xe1zquez (6 P.), Guerrero, Deyverson'
 
 nombre_comunidad = u'LaLiga12345 Noticias de la comunidad'
 
