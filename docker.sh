f91aab3052f9c8b18d0112ca3ca8ae0a48cee600
Sergio Cáceres <sergiocaceres@correo.ugr.es>
2016-11-29 13:14:33 +0100

    Corrigiendo erratas

--- docker.sh
+++ docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#Tras ejecutar el script no olvidar de ejecutar "cd IV && make run"
-
 #Descarga la imagen
 sudo docker pull sergiocaceres/iv
 #Ejecuta la imagen
 sudo docker run -i -t sergiocaceres/iv /bin/bash 
+
+#Tras ejecutar el script ejecutar "cd IV && make execute"
