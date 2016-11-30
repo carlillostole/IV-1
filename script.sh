618356fe9c73c6465c12b84de8caf55d178b788f
Sergio Caceres <sergiocaceres@correo.ugr.es>
2016-11-30 14:02:30 +0100

    Añadiendo script

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+#Descargar la imagen
+sudo docker pull sergiocaceres/iv
+
+#Ejecutar la imagen
+sudo docker run -i -t sergiocaceres/iv /bin/bash
+
+#Una vez ejecutado este script, tendremos que ejecutar "cd IV && make execute"
+
