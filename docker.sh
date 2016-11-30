8ddf280359964b1828286689479c41ac37e9a59c
Sergio Caceres <sergiocaceres@correo.ugr.es>
2016-11-30 10:10:06 +0100

    Añadiendo descargar Docker automaticamente closes #30

--- docker.sh
+++ docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#Descarga docker
+sudo apt-get update
+sudo apt-get install -y docker.io
+# Inicia el servicio docker
+sudo service docker start
+sudo docker -d &
 #Descarga la imagen
 sudo docker pull sergiocaceres/iv
 #Ejecuta la imagen
