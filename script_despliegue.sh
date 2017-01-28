ed25bf69a7cbc0b1791ae1372b4bbfa89e0718c8
Sergio Caceres <sergiocaceres@correo.ugr.es>
2017-01-28 21:52:40 +0100

    Fichero de despliegue automático closes #39

---
+++ script_despliegue.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# Instalamos vagrant
+sudo wget https://releases.hashicorp.com/vagrant/1.8.6/vagrant_1.8.6_i686.deb
+sudo dpkg -i vagrant_1.8.6_i686.deb
+
+# Instalar plugin para azure
+sudo vagrant plugin install vagrant-azure
+
+# Instalación Ansible
+sudo apt-get install ansible
+
+
+# Despliegue en Azure
+sudo vagrant up --provider=azure
+
+#Instalamos Fabric
+sudo apt-get install fabric
+
+# Despliegue de la aplicación con Fabric
+
+# Actualiza el supervisor
+fab -p Abecedario1234# -H sergio@comunibot.cloudapp.net recargar
+#Inicia el supervisor
+fab -p Abecedario1234# -H sergio@comunibot.cloudapp.net iniciar
+
